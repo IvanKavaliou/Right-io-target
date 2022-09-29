6914a0706cea23ecbc9fee8908d343f663be53a4
Ivan Kavaliou <luckucat@gmail.com>
2022-09-29 22:34:08 +0200

    Implement controller

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var label: UILabel!
+    
+    var number: Int = 0
+    var round: Int = 0
+    var scores: Int = 0
+    
+    
+    @IBAction func checkButtonClick(){
+        if round == 0 {
+            number = getRandomNumber()
+            label.text = String(number)
+            round = 1
+        } else {
+            let numOnSlider = Int(slider.value.rounded())
+            
+            if numOnSlider > number {
+                scores += 50 - numOnSlider + number
+            } else if numOnSlider < number {
+                scores += 50 - number + numOnSlider
+            } else {
+                scores += 50
+            }
+            
+            if round == 5 {
+                let alert = UIAlertController(title: "Game is over", message: "Your scores \(scores)", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Play again", style: .default))
+                present(alert, animated: true, completion: nil )
+                resetScores()
+            } else {
+                round += 1
+            }
+            
+            number = getRandomNumber()
+            label.text = String(number)
+        }
+        
     }
-
+    
+    func resetScores(){
+        round = 1
+        scores = 0
+    }
+    
+    func getRandomNumber() -> Int {
+        return Int.random(in: 1...50)
+    }
 
 }
 
